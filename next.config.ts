7b490c2869bf3c07cd1571ea4d4c90d5176d6dc9
Prakhyath S L <prakhyathsl77@gmail.com>
2025-12-09 22:01:09 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  eslint:{
-    ignoreDuringBuilds:true,
-    },
-     typescript: {
+    /* config options here */
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       protocol: "https",
+    //       hostname: "ik.imagekit.io",
+    //       port: "",
+    //     },
+    //   ],
+    // },
+    typescript: {
         ignoreBuildErrors: true,
-    }
+    },
 };
 
 export default nextConfig;
